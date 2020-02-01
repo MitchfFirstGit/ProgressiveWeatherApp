996acc952b869439e34d537250c32c66cdb67846
Mitch <artfixmitch@gmail.com>
2020-02-01 20:09:33 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,10 +1,17 @@
 /* eslint-disable */
 if ("function" === typeof importScripts) {
-    importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
+  importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
-    if (workbox) {
-        console.log("Workbox is loaded");
-        workbox.precaching.precacheAndRoute([
+  if (workbox) {
+    console.log("Workbox is loaded");
+
+    workbox.setConfig({ debug: false });
+
+    workbox.precaching.precacheAndRoute([
+  {
+    "url": "asset-manifest.json",
+    "revision": "a83865fa29fad95a4cbb58ea0e315bae"
+  },
   {
     "url": "img/Icon-120.png",
     "revision": "e68fdc591dcf0f5cd35f516f4b641263"
@@ -55,39 +62,87 @@ if ("function" === typeof importScripts) {
   },
   {
     "url": "index.html",
-    "revision": "3c877312d3281f8892cb8455ffd92cf3"
+    "revision": "5ff152eb53424e71f70e216109208e82"
   },
   {
-    "url": "logo192.png",
-    "revision": "581fa1d82b7152e685510b51d48edd3e"
+    "url": "manifest.json",
+    "revision": "59d485adfbd8d6606a4a475041612360"
   },
   {
-    "url": "logo512.png",
-    "revision": "260d57586012b0ed1ae78accc0bf7083"
-  },
-  {
-    "url": "precache-manifest.5fc695904164b6edf3fa7b8c7cb72f2e.js",
-    "revision": "5fc695904164b6edf3fa7b8c7cb72f2e"
+    "url": "precache-manifest.18e662cad2313744146a57d9461937d2.js",
+    "revision": "18e662cad2313744146a57d9461937d2"
   },
   {
     "url": "service-worker.js",
-    "revision": "9dc9927999bf097b70fccf459e5b40aa"
+    "revision": "e55c79850c5c70221038ecfe90bdb1cb"
+  },
+  {
+    "url": "splashscreens/ipad_splash.png",
+    "revision": "81d30f92d0a2b0bc49143fe7bf156654"
+  },
+  {
+    "url": "splashscreens/ipadpro1_splash.png",
+    "revision": "0b5c3df1c7fa51baeccae9124e740afb"
+  },
+  {
+    "url": "splashscreens/ipadpro2_splash.png",
+    "revision": "8df15c25c48492fe7065546bd43f611d"
+  },
+  {
+    "url": "splashscreens/ipadpro3_splash.png",
+    "revision": "0f869392d901dda068cdea357a88d2e0"
+  },
+  {
+    "url": "splashscreens/iphone5_splash.png",
+    "revision": "230cbd2934f501e9e3f248d42faad9c4"
+  },
+  {
+    "url": "splashscreens/iphone6_splash.png",
+    "revision": "22655f97707fcbfbb90329718b6fa270"
+  },
+  {
+    "url": "splashscreens/iphoneplus_splash.png",
+    "revision": "69ad8e7c5e85f5acff795a9e31f5213c"
+  },
+  {
+    "url": "splashscreens/iphonex_splash.png",
+    "revision": "9fb80e2f231692374ba6c24266c507e1"
+  },
+  {
+    "url": "splashscreens/iphonexr_splash.png",
+    "revision": "31e5375d7848e165aef72d6ad9ba0a89"
+  },
+  {
+    "url": "splashscreens/iphonexsmax_splash.png",
+    "revision": "ec20592b0ecdc15848f7c263cf6e2446"
   },
   {
     "url": "static/css/main.5c46fca8.chunk.css",
     "revision": "ccc798898ad2968a359c8ae680ad4870"
   },
   {
-    "url": "static/js/2.8a25c74a.chunk.js",
-    "revision": "3858b9bd0de76c0593e55e7bad06830d"
+    "url": "static/js/2.783f24fe.chunk.js",
+    "revision": "47529bcc551eeca49f6b4341d9b82e09"
   },
   {
-    "url": "static/js/main.db2f92b0.chunk.js",
-    "revision": "e3101e5866e23a8fe542d4d98b52aab2"
+    "url": "static/js/main.fe7f5f05.chunk.js",
+    "revision": "66e92fc94d82ac136cd8596a3d191276"
   },
   {
     "url": "static/js/runtime-main.8aff1fc4.js",
     "revision": "e72f3acef871d6ef7cc1b340bf35a0f5"
+  },
+  {
+    "url": "static/media/weathericons-regular-webfont.1cd48d78.woff2",
+    "revision": "1cd48d78f06d33973d9d761d426e69bf"
+  },
+  {
+    "url": "static/media/weathericons-regular-webfont.4618f0de.ttf",
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a"
+  },
+  {
+    "url": "static/media/weathericons-regular-webfont.8cac70eb.woff",
+    "revision": "8cac70ebda3f23ce472110d9f21e8593"
   },
   {
     "url": "static/media/weathericons-regular-webfont.ecaf8b48.svg",
@@ -95,33 +150,33 @@ if ("function" === typeof importScripts) {
   }
 ]);
 
-        workbox.routing.registerRoute(
-            /\.(?:woff2|woff|ttf)$/,
-            workbox.strategies.CacheFirst({
-              cacheName: "icons",
-              plugins: [
-                new workbox.expiration.Plugin({
-                  maxEntries: 60,
-                  maxAgeSeconds: 20 * 24 * 60 * 60 // 20 Days
-                })
-              ]
-            })
-          );
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg|svg)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: "images",
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+          })
+        ]
+      })
+    );
 
-        workbox.routing.registerRoute(
-            /\.(?:|js|css|png|gif|jpg|jpeg|svg)$/,
-            workbox.strategies.staleWhileRevalidate({
-                cacheName: "cashe-resources",
-                plugins: [
-                    new workbox.expiration.Plugin({
-                        maxEntries: 80,
-                        maxAgeSeconds: 30 * 24 * 60 * 60
-                    })
-                ]
-            })
-        );
+    workbox.routing.registerRoute(
+      /\.(?:js|css)$/,
+      workbox.strategies.staleWhileRevalidate({
+        cacheName: "static-resources",
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 80,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+          })
+        ]
+      })
+    );
 
-    } else {
-        console.error("Workbox could not be loaded. No offline support");
-    }
+  } else {
+    console.error("Workbox could not be loaded. No offline support");
+  }
 }
